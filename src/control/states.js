f31fde8fbf2df58eb928040009ec6e27a0309896
ginnko <yybnbn@126.com>
2018-05-14 23:54:58 +0800

    read code 2
    1. continue want in index.js in until
    2. continue auto in states.js in control
    3. continue componentDidMount in index.js in containers

--- src/control/states.js
+++ src/control/states.js
@@ -133,6 +133,7 @@ const states = {
       return;
     }
     const state = store.getState();
+    console.log(state);
     if (state.get('cur') && !state.get('reset') && !state.get('pause')) {
       states.auto();
     }
